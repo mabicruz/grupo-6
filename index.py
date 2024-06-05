c5f4349fec4da90df8abc06f5bbbc65ec68182cc
luquilla84 <134623292+luquilla84@users.noreply.github.com>
2024-06-05 20:07:22 -0300

    Melodist

---
+++ index.py
@@ -0,0 +1,27 @@
+def main_menu(): #
+    while True:
+        print("\nMenu de opciones:")
+        print("1. Crear")
+        print("2. Leer")
+        print("3. Actualizar")
+        print("4. Eliminar")
+        print("5. Salir")
+        
+        choice = input("Seleccione una opción: ")
+        
+        if choice == '1':
+            print("Opción Crear seleccionada")
+        elif choice == '2':
+            print("Opción Leer seleccionada")
+        elif choice == '3':
+            print("Opción Actualizar seleccionada")
+        elif choice == '4':
+            print("Opción Eliminar seleccionada")
+        elif choice == '5':
+            print("Saliendo...")
+            break
+        else:
+            print("Opción no válida. Por favor, elija otra.")    
+  
+main_menu()
+
