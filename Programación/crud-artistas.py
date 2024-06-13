e9f542d61d6eefff11256df7bf281395ac5edf86
Nahir Ferreyra <nahirfdubouloy@gmail.com>
2024-06-12 22:03:46 -0300

    crud-artistas

---
+++ Programación/crud-artistas.py
@@ -0,0 +1,56 @@
+import mysql.connector 
+import os
+
+class App:
+    
+    def __init__(self):
+        self.conexion = mysql.connector.connect(
+            user= "root", 
+            password="tiamijuamax95", 
+            host="localhost", 
+            database="melodist", 
+            port="3306"
+            )
+        self.cursor = self.conexion.cursor()
+        print("========================================================================")
+        print("Conexion realizada exitosa")
+        
+
+    def Insertar(self):
+        print("========================================================================")
+        print("-------------------CREAR ARTISTA----------------------------------------")
+        print("========================================================================")
+        nombre = input("Ingrese el nombre del artista: \n")
+        sql = "insert into artistas(nombre) values('{}')".format(nombre)
+        self.cursor.execute(sql)
+        print("nombre del artista ingresado correctamete \n")
+        self.conexion.commit()
+        os.system('pause')
+        
+    def Mostrar(self):
+        print("========================================================================")
+        print("-------------------LISTA DE ASTISTAS------------------------------------")
+        print("========================================================================")
+        sql= "select * from artistas"
+        self.cursor.execute(sql)
+        artistas = self.cursor.fetchall()
+        """ print(artistas) """
+        for i in artistas:
+            print("Nombre del artista: ", i[1])
+            
+    def Actualizar(self):
+        print("========================================================================")
+        print("-------------------EDITAR ARTISTA---------------------------------------")
+        print("========================================================================")
+        nombre = input("Ingrese el nombre del artista a editar \n")
+        nuevo_nombre = input("Ingrese el nuevo nombre \n")
+        sql = "update artistas set nombre = '{}' where nombre= '{}'".format(nuevo_nombre, nombre)
+        self.cursor.execute(sql)
+        print("Artista actualizado! \n")
+        self.conexion.commit()
+        
+app = App()
+app.Insertar() 
+app.Mostrar() 
+app.Actualizar() 
+app.Mostrar()
